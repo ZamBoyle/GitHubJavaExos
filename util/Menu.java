51a5f1d9580f1b69c32d270cd5a04bda889fcd7d
Johnny Piette <johnny.piette@gmail.com>
2022-07-08 01:41:31 +0200

    Mise à jour commentaire Menu.java

--- util/Menu.java
+++ util/Menu.java
@@ -1,7 +1,20 @@
 package util;
 
 public class Menu {
-    public static void AfficheMenu(){
-        //Afficher un menu de 5 choix numérotés: l'addition de deux nombres, la soustraction, la multiplication, la division, le carré
+    public static void AfficheMenu() {
+        /*
+          Afficher un menu de 5 choix numérotés: l'addition de deux nombres, la
+          soustraction, la multiplication, la division, le carré
+          Il ressemblera à ceci mais c'est à titre d'exemple
+          
+          =====================================
+          = MENU DES OPERATIONS ARITHMÉTIQUES =
+          =====================================
+          1) Additionner deux nombres.
+          2) Soustraire deux nombres.
+          3) Multiplier deux nombres.
+          4) Diviser deux nombres.
+          5) Le carré d'un nombre.
+         */
     }
 }
