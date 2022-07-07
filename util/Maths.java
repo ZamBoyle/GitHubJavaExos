f3dcddc35e61c31325cc354dd51bc9bcd9da57a7
Johnny Piette <johnny.piette@gmail.com>
2022-07-08 01:15:38 +0200

    Ajout en commentaires des fonctions à implémenter

--- util/Maths.java
+++ util/Maths.java
@@ -5,4 +5,11 @@ public class Maths {
     public static float add(float number1, float number2) {
         return number1 + number2;
     }
+    /* 
+        Ajoutez les function
+        - substract
+        - multiply
+        - divide
+        - square
+    */
 }
