94b87b68912816a53427eb71193f3bf095361ba9
Sebastien Baloge <s.baloge@yahoo.fr>
2022-07-14 11:06:20 +0200

    modification du fichier maths.java

--- util/Maths.java
+++ util/Maths.java
@@ -5,8 +5,20 @@ public class Maths {
     public static float add(float number1, float number2) {
         return number1 + number2;
     }
+    public static float substract (float number1, float number2) {
+return number1 - number2;        
+    }
+    public static float multiply (float number1, float number2) {
+        return number1 * number2;
+    }
+public static float divide (float number1, float number2)  {
+    return number1 / number2;
+}
+public static float square  (float number1) {
+return number1 * number1;
+}      
     /* 
-        Ajoutez les function
+        Ajoutez les functqion
         - substract
         - multiply
         - divide
